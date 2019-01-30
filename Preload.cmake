c03bbb8779e02b84b93862ee2468ae5db0eadbeb
VT <personwithhats2@Gmail.com>
2019-01-29 17:54:50 -0800

    TEMP - Local change for faster debugging :)

--- Preload.cmake
+++ Preload.cmake
@@ -4,8 +4,8 @@
 
 ## Required to build GUI
 set(BUILD_QtDialog ON CACHE BOOL "" FORCE)
-set(CMAKE_PREFIX_PATH "C:\\Qt\\5.12.0\\msvc2017_64" CACHE STRING "" FORCE)
+set(CMAKE_PREFIX_PATH "B:\\Qt\\5.12.0\\msvc2017_64" CACHE STRING "" FORCE)
 
 ## Standardizing build options for 64-bit CMake
 set(CMAKE_GENERATOR "Visual Studio 15 2017 Win64" CACHE INTERNAL "" FORCE)
-set(CMAKE_INSTALL_PREFIX "${CMAKE_CURRENT_LIST_DIR}/standalone" CACHE STRING "" FORCE)
+set(CMAKE_INSTALL_PREFIX "D:\\GitHub\\CRYENGINE_OFFICIAL\\Tools\\CMake\\Win64" CACHE STRING "" FORCE)
